17d29eef6130a9b6b12a37c06e50b73cbd72e2b0
Perilous-Programmer <softengrabdullah@gmail.com>
2022-10-13 22:49:42 -0700

    complete

---
+++ main.rb
@@ -0,0 +1,49 @@
+require_relative 'visualcrossing'
+require 'date'
+require 'terminal-table'
+
+currentDate = DateTime.now.strftime "%Y-%m-%d"
+lastMonthDate = DateTime.now.prev_month.strftime "%Y-%m-%d"
+
+
+def median(array)
+    sorted_array = array.sort 
+    count = sorted_array.count 
+
+    if sorted_array.count % 2 == 0 
+    first_half = (sorted_array[0...(count/2)])
+    second_half = (sorted_array[(count/2)..-1])
+
+    first_median = first_half[-1]
+    second_median = second_half[0]
+
+    true_median = ((first_median + second_median).to_f / 2.to_f)
+    true_median
+    else 
+    true_median = sorted_array[(count/2).floor]
+    true_median
+    end 
+end
+
+
+cityAddressList = [
+    'Copenhagen, Denmark',
+    'Lodz, Poland',
+    'Brussels, Belgium',
+    'Islamabad, Pakistan'
+] 
+
+temp = VisualCrossing.new
+rows = [["city", "wind_avg", "wind_median", "temp_avg", "temp_median"]]
+cityAddressList.map { |address|
+temp.selectCityByAddress(cityAddressList.first)
+days =  temp.getPastDaysWithTemperature(lastMonthDate, currentDate)
+dayWind = days.map {|day| day["windspeed"]}
+# dayWind = [16.5, 15.1, 13.6, 12.3]
+dayTemp = days.map {|day| day["temp"]}
+# dayTemp = [16.5, 15.1, 13.6, 12.3]
+rows << [address,dayWind.sum/2,median(dayWind), dayTemp.sum/2, median(dayTemp)]
+}
+
+table = Terminal::Table.new :rows => rows
+puts table
--- visualcrossing.rb
+++ visualcrossing.rb
@@ -1,9 +1,29 @@
+require 'net/http'
+require 'json'
+
 class VisualCrossing
-    def initialize(key,cityName)
+    @@key = 'Z7KHG7S28BDP5RB6B3MBEZD3E'
+    @@apiUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline'
+    @@apiParams = "unitGroup=metric&include=days&key=#{@@key}&contentType=json"
+
+    def initialize()
         
     end
 
-    def getPastTemperature(fromDate, toDate)
-        
+    def selectCityByAddress(address)
+        @address = address
+    end
+
+    def getPastDaysWithTemperature(fromDate, toDate)
+        parser = URI::Parser.new
+        uri = URI(parser.escape("#{@@apiUrl}/#{@address}/#{fromDate}/#{toDate}?#{@@apiParams}"))
+        res = Net::HTTP.get_response(uri)
+
+        return  JSON.parse(res.body)["days"] if res.is_a?(Net::HTTPSuccess)
+
+        return  res.body
+
+    rescue Exception => e
+        return e.message
     end
 end
